97351af0272ad112eab68b216c224500bfe4ae5d
Pavin Wu <pavin.wu@gmail.com>
2026-01-11 14:46:29 +1100

    Add oled_handler.js

---
+++ oled_handler.js
@@ -0,0 +1,30 @@
+const i2c = require('i2c-bus');
+let i2cBus = i2c.openSync(1);
+let oledLib = require('oled-i2c-bus');
+
+function init()
+{
+    let opts = {
+        width: 128,
+        height: 64,
+        address: 0x3C
+    }
+    
+    let oled = new oledLib(i2cBus, opts);
+    oled.turnOnDisplay();
+    oled.clearDisplay();
+    
+    return oled;
+}
+
+function writeText(oled, text, line)
+{
+
+}
+
+const oledHandler = {
+    init: init,
+    writeText: writeText
+}
+
+module.exports = oledHandler;
